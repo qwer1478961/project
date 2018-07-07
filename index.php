64701601c1d7f2b444350df312ebaa5c2ae53251
qwer1478961 <39252927+qwer1478961@users.noreply.github.com>
2018-07-07 22:27:46 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 		帳號:<input type="text" name="UserID"/><br>	
 		密碼:<input type="password" name="Passwd"/><br>
 		<input type="submit" name="button" value="登入"/>&nbsp;&nbsp;
-		<a href="register.php">申請帳號</a>		//連結
+		<a href="register.php">申請帳號</a>		
 	<form/>
 	
